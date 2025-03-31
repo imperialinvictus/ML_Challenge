f2a5ac8658a6091d495e730ec647881886ce9816
leowrites <leo@liuwork.ca>
2025-03-30 23:56:48 -0400

    make pred.py more extensible

--- nn/pred.py
+++ nn/pred.py
@@ -5,29 +5,33 @@ import sys
 sys.path.insert(0, '..')
 from text_cluster.standalone_data_clean import get_dataframe_from_file
 
-
-def predict_all(filename):
+def predict_all(filename, models):
     """
-    Make predictions for the data in filename
+    Make predictions for the data in filename using a list of models
     """
-    nn = NN('models/mlp_model.npz')
-    baggedNN = BaggedNN('models/normBagged_model.npz')
-
     inputs = get_dataframe_from_file(filename, "../text_cluster/", has_labels=False)
-    predictions = nn.predict(inputs)
-    baggedPredictions = baggedNN.predict(inputs)
+    predictions = {}
     
-    expected = pd.read_csv('dataset/ai_test_y.csv') # TODO: delete when submitting
-    baseCorrect = sum(predictions[i] == expected.iloc[i, 1] for i in range(len(predictions)))
-    baseAccuracy = baseCorrect / len(predictions)
-    baggedCorrect = sum(baggedPredictions[i] == expected.iloc[i, 1] for i in range(len(predictions)))
-    baggedAccuracy = baggedCorrect / len(predictions)
-    
-    print(f"Base Accuracy: {baseAccuracy:.2f}")
-    print(f"Bagged Accuracy: {baggedAccuracy:.2f}")
+    for model_name, model_path in models.items():
+        model = NN(model_path) if 'bagged' not in model_name.lower() else BaggedNN(model_path)
+        model_predictions = model.predict(inputs)
+        predictions[model_name] = model_predictions
+
+    # TODO: delete the following block when submitting
+    expected = pd.read_csv('dataset/ai_test_y.csv')
+    for model_name, model_predictions in predictions.items():
+        correct = sum(model_predictions[i] == expected.iloc[i, 1] for i in range(len(model_predictions)))
+        accuracy = correct / len(model_predictions)
+        print(f"{model_name} Accuracy: {accuracy:.2f}")
     
     return predictions
-    
+
 
 if __name__ == '__main__':
-    predict_all('dataset/ai_test.csv')
+    models = {
+        "BaseNN": "models/mlp_model.npz",
+        "BaggedNN": "models/bagged_model.npz",
+        "NormBaseNN": "models/normMlp_model.npz",
+        "NormBaggedNN": "models/normBagged_model.npz"
+    }
+    predict_all('dataset/ai_test.csv', models)
